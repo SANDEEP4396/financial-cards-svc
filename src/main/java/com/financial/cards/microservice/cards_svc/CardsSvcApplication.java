393abffd847cf3c763de8c2751d907ef71039277
sandeep4396 <sandeep.sagar033@gmail.com>
2026-02-11 23:41:15 -0600

    Create cards project

---
+++ src/main/java/com/financial/cards/microservice/cards_svc/CardsSvcApplication.java
@@ -0,0 +1,13 @@
+package com.financial.cards.microservice.cards_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financial/cards/microservice/cards_svc/CardsSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financial.cards.microservice.cards_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
