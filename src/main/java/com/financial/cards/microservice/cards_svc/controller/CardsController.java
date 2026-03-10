924ebf2e2929704c6c516fc863b85acde021f725
sandeep4396 <sandeep.sagar033@gmail.com>
2026-03-10 18:49:03 -0500

    Configure routes and clean up

--- src/main/java/com/financial/cards/microservice/cards_svc/controller/CardsController.java
+++ src/main/java/com/financial/cards/microservice/cards_svc/controller/CardsController.java
@@ -33,7 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
         description = "CRUD REST APIs to CREATE, UPDATE, FETCH AND DELETE card details"
 )
 @RestController
-@RequestMapping(path = "/api/cards", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
 public class CardsController {
 
