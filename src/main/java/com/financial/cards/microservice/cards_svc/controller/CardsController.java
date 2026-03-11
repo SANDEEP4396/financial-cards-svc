f90b83a0e1eb1be784009030f477977524decbc1
sandeep4396 <sandeep.sagar033@gmail.com>
2026-03-10 21:12:06 -0500

    Config gateway

--- src/main/java/com/financial/cards/microservice/cards_svc/controller/CardsController.java
+++ src/main/java/com/financial/cards/microservice/cards_svc/controller/CardsController.java
@@ -14,6 +14,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -24,6 +26,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +40,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 public class CardsController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CardsController.class);
     private final ICardsService iCardsService;
     @Autowired
     private CardsContactInfoDto cardsContactInfoDto;
@@ -91,9 +95,11 @@ public class CardsController {
             )
     })
     @GetMapping("/fetch")
-    public ResponseEntity<CardDTO> fetchCardDetails(@RequestParam
+    public ResponseEntity<CardDTO> fetchCardDetails(@RequestHeader("X-Correlation-ID") String correlationId,
+                                                    @RequestParam
                                                     @Pattern(regexp = "^[2-9][0-9]{9}$", message = "Phone number must be 10 digits")
                                                     String phoneNumber) {
+        LOGGER.debug("CorrelationId: {}, Fetching cards details for phone number: {}", correlationId, phoneNumber);
         CardDTO cardsDto = iCardsService.fetchCard(phoneNumber);
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
